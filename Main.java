2b57378b4e1bbb800bfdace9270cf910e02d7b58
juoliver1 <107935617+juoliver1@users.noreply.github.com>
2022-11-29 10:50:09 -0300

    atividadefinal

--- Main.java
+++ Main.java
@@ -1,76 +1,14 @@
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        Scanner scanner = new Scanner(System.in);
-
-        int opcao = -1;
-
-        double numero1, numero2;
-        while (true) {
-            System.out.print("Digite uma opção (1-soma, 2-subtracao, 3-multiplicacao, 4-divisao, 5-exponencial, 6-raiz, 7-piso, 8-teto, 0-sair): ");
-            opcao = scanner.nextInt();
-
-
-            if (opcao == 0)
-                break;
-            if(opcao == 6 || opcao == 7 || opcao == 8 ){
-
-                System.out.println("Digite um numero : ");
-                numero1 = scanner.nextDouble();
-                numero2 = 0;
-            } else {
-
-                System.out.print("Digite o primeiro numero: ");
-                numero1 = scanner.nextDouble();
-                System.out.print("Digite o segundo numero: ");
-                numero2 = scanner.nextDouble();
-            }
-
-
-
-            switch (opcao) {
-                case 1: {
-                    System.out.println(calculadora.soma(numero1, numero2));
-                    break;
-                }
-                case 2: {
-                    System.out.println(calculadora.subtracao(numero1, numero2));
-                    break;
-                }
-                case 3: {
-                    System.out.println(calculadora.multiplicacao(numero1, numero2));
-                    break;
-                }
-                case 4: {
-                    System.out.println(calculadora.divisao(numero1, numero2));
-                    break;
-                }
-                case 5: {
-                    System.out.println(calculadora.exponencial(numero1, numero2));
-                    break;
-                }
-                case 6: {
-                    System.out.println(calculadora.raiz(numero1));
-                    break;
-                }
-                case 7: {
-                    System.out.println(calculadora.piso(numero1));
-                    break;
-                }
-                case 8: {
-                    System.out.println(calculadora.teto(numero1));
-                    break;
-                }
-
-                default:
-                    System.out.println("Digite um número entre 0 e 8");
-                    break;
-            }
-
-        }
-        scanner.close();
+		System.out.println("A soma do número 250 com o número 50 é: " + calculadora.soma(250, 50));
+		System.out.println("A subtração do número 400 com o número 200 é: " + calculadora.subtracao(400, 200));
+		System.out.println("A divisão do número 30 por 5 é: " + calculadora.divisao(30, 5));
+		System.out.println("A multiplicação do número 50 com o número 2 é: " + calculadora.multiplicacao(50,2));
+		System.out.println("A raiz quadrada de 36 é: " + calculadora.raiz(36));
+		System.out.println("O resultado de 2 elevado a 3 é: " + calculadora.exponencial(2, 3));
     }
 }
 
